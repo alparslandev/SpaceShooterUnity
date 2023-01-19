b3c1f2d1b8530de71b8bf0655e60f7cf1a6a7b05
AlparslanDev <alp.develioglu@gmail.com>
2023-01-19 03:56:51 +0300

    Asteroid and its explosion animation is added

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject, 0.5f);
+            Destroy(gameObject);
+        }
+    }
+}
